9ec1f54c3dfbfdad7d85f4d8454a0158c04b8b2d
Shreya <prappo.prince@gmail.com>
2023-02-22 15:16:19 +0600

    few update

--- src/partials/Footer.jsx
+++ src/partials/Footer.jsx
@@ -1,27 +1,133 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Logo from '../images/logo.svg';
 
 function Footer() {
   return (
     <footer>
       <div className="max-w-6xl mx-auto px-4 sm:px-6">
 
+        {/* Top area: Blocks */}
+        <div className="grid sm:grid-cols-12 gap-8 py-8 md:py-12 border-t border-gray-200">
+
+          {/* 1st block */}
+          <div className="sm:col-span-12 lg:col-span-3">
+            <div className="mb-2">
+              {/* Logo */}
+              <Link to="/" className="inline-block" aria-label="Cruip">
+                <img className='w-12' src={Logo} />
+              </Link>
+            </div>
+            <div className="text-sm text-gray-600">
+              <Link to="#" className="text-gray-600 hover:text-gray-900 hover:underline transition duration-150 ease-in-out">Terms</Link> · <Link to="#" className="text-gray-600 hover:text-gray-900 hover:underline transition duration-150 ease-in-out">Privacy Policy</Link>
+            </div>
+          </div>
+
+          {/* 2nd block */}
+          <div className="sm:col-span-6 md:col-span-3 lg:col-span-2">
+            <h6 className="text-gray-800 font-medium mb-2">Products</h6>
+            <ul className="text-sm">
+              <li className="mb-2">
+                <Link to="#" className="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out">Template Editor</Link>
+              </li>
+              <li className="mb-2">
+                <Link to="#" className="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out">Visuals generation</Link>
+              </li>
+             
+            </ul>
+          </div>
+
+          {/* 3rd block */}
+          <div className="sm:col-span-6 md:col-span-3 lg:col-span-2">
+            <h6 className="text-gray-800 font-medium mb-2">Resources</h6>
+            <ul className="text-sm">
+              <li className="mb-2">
+                <Link to="#" className="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out">Documentation</Link>
+              </li>
+              <li className="mb-2">
+                <Link to="#" className="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out">Tutorials & Guides</Link>
+              </li>
+              <li className="mb-2">
+                <Link to="#" className="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out">Blog</Link>
+              </li>
+             
+            </ul>
+          </div>
+
+          {/* 4th block */}
+          <div className="sm:col-span-6 md:col-span-3 lg:col-span-2">
+            <h6 className="text-gray-800 font-medium mb-2">Company</h6>
+            <ul className="text-sm">
+              
+              <li className="mb-2">
+                <Link to="#" className="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out">Pricing</Link>
+              </li>
+              <li className="mb-2">
+                <Link to="#" className="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out">Privacy Policy</Link>
+              </li>
+            </ul>
+          </div>
+
+          {/* 5th block */}
+          <div className="sm:col-span-6 md:col-span-3 lg:col-span-3">
+            <h6 className="text-gray-800 font-medium mb-2">Subscribe</h6>
+            <p className="text-sm text-gray-600 mb-4">Get the latest news and articles to your inbox every month.</p>
+            <form>
+              <div className="flex flex-wrap mb-4">
+                <div className="w-full">
+                  <label className="block text-sm sr-only" htmlFor="newsletter">Email</label>
+                  <div className="relative flex items-center max-w-xs">
+                    <input id="newsletter" type="email" className="form-input w-full text-gray-800 px-3 py-2 pr-12 text-sm" placeholder="Your email" required />
+                    <button type="submit" className="absolute inset-0 left-auto" aria-label="Subscribe">
+                      <span className="absolute inset-0 right-auto w-px -ml-px my-2 bg-gray-300" aria-hidden="true"></span>
+                      <svg className="w-3 h-3 fill-current text-blue-600 mx-3 flex-shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M11.707 5.293L7 .586 5.586 2l3 3H0v2h8.586l-3 3L7 11.414l4.707-4.707a1 1 0 000-1.414z" fillRule="nonzero" />
+                      </svg>
+                    </button>
+                  </div>
+                  {/* Success message */}
+                  {/* <p className="mt-2 text-green-600 text-sm">Thanks for subscribing!</p> */}
+                </div>
+              </div>
+            </form>
+          </div>
+
+        </div>
+
         {/* Bottom area */}
         <div className="md:flex md:items-center md:justify-between py-4 md:py-8 border-t border-gray-200">
 
           {/* Social links */}
           <ul className="flex mb-4 md:order-1 md:ml-4 md:mb-0">
-            
-            <li className="ml-4">
-              <a target="_blank" href="https://www.youtube.com/@labelbaker" className="flex justify-center items-center text-gray-600 hover:text-gray-900 bg-white hover:bg-white-100 rounded-full shadow transition duration-150 ease-in-out" aria-label="Youtube">
-
-                <svg className="w-8 h-8 fill-current" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" fill="none"><path fill="#000000" fillRule="evenodd" d="M13.478 3.399c.6.161 1.072.634 1.234 1.234C15 5.728 15 8 15 8s0 2.272-.288 3.367a1.754 1.754 0 01-1.234 1.234C12.382 12.89 8 12.89 8 12.89s-4.382 0-5.478-.289a1.754 1.754 0 01-1.234-1.234C1 10.283 1 8 1 8s0-2.272.288-3.367c.162-.6.635-1.073 1.234-1.234C3.618 3.11 8 3.11 8 3.11s4.382 0 5.478.289zm-3.24 4.612l-3.645 2.1V5.9l3.644 2.11z" clipRule="evenodd" /></svg>
+            <li>
+              <a href="https://twitter.com/labelbaker" target={'_blank'} className="flex justify-center items-center text-gray-600 hover:text-gray-900 bg-white hover:bg-white-100 rounded-full shadow transition duration-150 ease-in-out" aria-label="Twitter">
+                <svg className="w-8 h-8 fill-current" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M24 11.5c-.6.3-1.2.4-1.9.5.7-.4 1.2-1 1.4-1.8-.6.4-1.3.6-2.1.8-.6-.6-1.5-1-2.4-1-1.7 0-3.2 1.5-3.2 3.3 0 .3 0 .5.1.7-2.7-.1-5.2-1.4-6.8-3.4-.3.5-.4 1-.4 1.7 0 1.1.6 2.1 1.5 2.7-.5 0-1-.2-1.5-.4 0 1.6 1.1 2.9 2.6 3.2-.3.1-.6.1-.9.1-.2 0-.4 0-.6-.1.4 1.3 1.6 2.3 3.1 2.3-1.1.9-2.5 1.4-4.1 1.4H8c1.5.9 3.2 1.5 5 1.5 6 0 9.3-5 9.3-9.3v-.4c.7-.5 1.3-1.1 1.7-1.8z" />
+                </svg>
               </a>
             </li>
+            <li className="ml-4">
+              <a href="https://www.youtube.com/@labelbaker" target={'_blank'} className="flex justify-center items-center text-gray-600 hover:text-gray-900 bg-white hover:bg-white-100 rounded-full shadow transition duration-150 ease-in-out" aria-label="Youtube">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="w-8 h-8 fill-current"
+                  viewBox="0 0 32 32"
+                >
+                  <path d="M24.325 8.309s-2.655-.334-8.357-.334c-5.517 0-8.294.334-8.294.334A2.675 2.675 0 005 10.984v10.034a2.675 2.675 0 002.674 2.676s2.582.332 8.294.332c5.709 0 8.357-.332 8.357-.332A2.673 2.673 0 0027 21.018V10.982a2.673 2.673 0 00-2.675-2.673zM13.061 19.975V12.03L20.195 16l-7.134 3.975z"></path>
+                </svg>
+              </a>
+            </li>
+            {/* <li className="ml-4">
+              <Link to="#" className="flex justify-center items-center text-gray-600 hover:text-gray-900 bg-white hover:bg-white-100 rounded-full shadow transition duration-150 ease-in-out" aria-label="Facebook">
+                <svg className="w-8 h-8 fill-current" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M14.023 24L14 17h-3v-3h3v-2c0-2.7 1.672-4 4.08-4 1.153 0 2.144.086 2.433.124v2.821h-1.67c-1.31 0-1.563.623-1.563 1.536V14H21l-1 3h-2.72v7h-3.257z" />
+                </svg>
+              </Link>
+            </li> */}
           </ul>
 
           {/* Copyrights note */}
-          <div className="text-sm text-gray-600 mr-4">Made by <a target="_blank" className="text-blue-600 hover:underline" href="https://twitter.com/prappo_p">Prappo</a>. All rights reserved.</div>
+          <div className="text-sm text-gray-600 mr-4">Made with love by <a className="text-blue-600 hover:underline" href="https://twitter.com/prappo_p">Prappo</a>. All rights reserved.</div>
 
         </div>
 
--- src/partials/Header.jsx
+++ src/partials/Header.jsx
@@ -15,9 +15,8 @@ function Header() {
 
   return (
     <header
-      className={`fixed w-full z-30 md:bg-opacity-90 transition duration-300 ease-in-out ${
-        !top && 'bg-white backdrop-blur-sm shadow-lg'
-      }`}
+      className={`fixed w-full z-30 md:bg-opacity-90 transition duration-300 ease-in-out ${!top && 'bg-white backdrop-blur-sm shadow-lg'
+        }`}
     >
       <div className="max-w-6xl mx-auto px-5 sm:px-6">
         <div className="flex items-center justify-between h-16 md:h-20">
@@ -25,14 +24,19 @@ function Header() {
           <div className="flex-shrink-0 mr-4">
             {/* Logo */}
             <Link to="/" className="block" aria-label="LabelBaker">
-             <img style={{width:'200px'}} src={Logo} />
+              <img style={{ width: '200px' }} src={Logo} />
             </Link>
           </div>
 
           {/* Site navigation */}
           <nav className="flex flex-grow">
             <ul className="flex flex-grow justify-end flex-wrap items-center">
-              <li></li>
+            <li>
+                <Link to="/products" className="font-medium text-gray-600 hover:text-gray-900 px-5 py-3 flex items-center transition duration-150 ease-in-out">Products</Link>
+              </li>
+              <li>
+                <Link to="/pricing" className="font-medium text-gray-600 hover:text-gray-900 px-5 py-3 flex items-center transition duration-150 ease-in-out">Pricing</Link>
+              </li>
               <li>
                 <a
                   target="_blank"
